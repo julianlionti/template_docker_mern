b69dae230b52d89d8a484f7337d5bca5c16adcfe
Julian Lionti <julliont@publicisgroupe.net>
2021-12-28 20:18:25 -0300

    monolitic backend

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -4,13 +4,10 @@ import api from "../routes/api.route";
 import morgan from "morgan";
 import Config from "../utils/config";
 import { connectDatabase } from "./db";
-import connectRabbitMq from "./rabbitmq";
 
 const configureServer = async () => {
   console.log("Configuring server");
   await connectDatabase();
-  // const rabbit = await connectRabbitMq();
-  // console.log(rabbit);
 
   const app = express();
   app.use(bodyParser.urlencoded({ extended: true }));
