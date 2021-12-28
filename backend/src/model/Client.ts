df6cbe3cd4d50c6cf2fc4d4fbd8c811671c3cc84
Julian Lionti <julliont@publicisgroupe.net>
2021-12-28 19:23:05 -0300

    preparing gateway

--- backend/src/model/Client.ts
+++
@@ -1,5 +0,0 @@
-export interface Client {
-  name: string;
-  email: string;
-  requirement: string;
-}
--- backend/src/routes/api.route.ts
+++ backend/src/routes/api.route.ts
@@ -1,18 +1,12 @@
 import { Router } from "express";
 import Config from "../utils/config";
-import swaggerUI from "swagger-ui-express";
-import swaggerJson from "../docs/swagger.json";
 
 const api = () => {
   const router = Router();
 
   router.get("/", (req, res) => {
-    res.send(
-      `<div><h1>${Config.APP_NAME} api</h1><a href="/api/swagger/v1">Go to documentation V1</a></div>`
-    );
+    res.send(`<div><h1>${Config.APP_NAME} api</h1></div>`);
   });
-
-  router.use("/swagger/v1", swaggerUI.serve, swaggerUI.setup(swaggerJson));
 
   return router;
 };
